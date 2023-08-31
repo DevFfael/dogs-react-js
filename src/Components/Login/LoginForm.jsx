185d4b3581510b464c4826dfb8b40b2d0d91de41
DevFfael <raffaeloliveirap@gmail.com>
2023-08-31 16:30:38 -0300

    feature - created register form and useFetch Hook

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
+import { Link } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
+import Error from '../Utils/Error';
+import styles from './LoginForm.module.css';
+import stylesBtn from '../Forms/Button.module.css';
 
 const LoginForm = () => {
   const username = useForm();
@@ -13,15 +17,14 @@ const LoginForm = () => {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      console.log('aqui');
       userLogin(username.value, password.value);
     }
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft">
+      <h1 className="title">Login</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
         {loading ? (
@@ -29,8 +32,19 @@ const LoginForm = () => {
         ) : (
           <Button>Entrar</Button>
         )}
+        <Error error={error} />
         {error && <p>{error}</p>}
       </form>
+      <Link className={styles.perdeu} to="/login/lost">
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link className={stylesBtn.button} to="/login/create">
+          Cadastro
+        </Link>
+      </div>
     </section>
   );
 };
