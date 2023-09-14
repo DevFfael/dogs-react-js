f2207366ab0ec0b8bd23add80a395140c8a9965c
DevFfael <raffaeloliveirap@gmail.com>
2023-09-14 02:19:15 -0300

    feature - create stats page

--- src/Components/User/UserStats.jsx
+++ src/Components/User/UserStats.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import Head from '../Utils/Head';
+import useFetch from '../../Hooks/useFetch';
+import Loading from '../Utils/Loading';
+import Error from '../Utils/Error';
+import { STATS_GET } from '../../Routes/api';
+
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title="Estastísticas" />
-      UserStats
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data) {
+    return (
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estastísticas" />
+        <UserStatsGraphs data={data} />
+      </React.Suspense>
+    );
+  } else return null;
 };
 
 export default UserStats;
